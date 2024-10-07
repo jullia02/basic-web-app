467ad42be13ca8f751b465c11411c9bcdbf78bb4
jullia andrei <jullia@jullias-air.wifi.local.cmu.edu>
2024-10-07 09:26:57 -0400

    test files.

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -16,5 +16,22 @@ describe("QueryProcessor", () => {
             "English poet, playwright, and actor, widely regarded as the greatest " +
             "writer in the English language and the world's pre-eminent dramatist."
           ));
+
     });
+
+    describe("QueryProcessor", () => {
+        test("should return a string", () => {
+            const query = "test";
+            const response: string = QueryProcessor(query);
+            expect(typeof response).toBe("string");
+        });
+
+    test('should return andrew id', () => {
+    const query = "id";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe((
+        "jmontejo"
+        ));
+ 
+}); });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,9 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("id")){
+    return ("jmontejo");
+  }
+
   return "";
 }
